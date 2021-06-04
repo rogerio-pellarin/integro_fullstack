59177b6d1cade8e2adab5bc2fe9bb844c325a3e3
Yoser Perez <yoserp1@gmail.com>
2021-06-04 13:50:58 -0500

    Test 1 php resuelto

--- test1.php
+++ test1.php
@@ -1,7 +1,63 @@
 <?php
 
+function eliminar_tildes($string){
+
+    //Ahora reemplazamos las letras
+    $string = str_replace(
+        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+        $string
+    );
+
+    $string = str_replace(
+        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+        $string );
+
+    $string = str_replace(
+        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+        $string );
+
+    $string = str_replace(
+        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+        $string );
+
+    $string = str_replace(
+        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+        $string );
+
+    $string = str_replace(
+        array('ñ', 'Ñ', 'ç', 'Ç'),
+        array('n', 'N', 'c', 'C'),
+        $string
+    );
+
+    return $string;
+}
+
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
+        //funcion eliminar tildes
+        $input = eliminar_tildes($input);
+        //remover espacios
+        $input = str_replace(' ', '', $input);
+        //remove caracter especial
+        $input = preg_replace('/[^A-Za-z0-9\-]/', '', $input);
+        //minusculas
+        $input = strtolower($input);
+        //reversar string
+        $reverse = strrev($input);
+        if ($input == $reverse) {
+            $respuesta = "Palindromo";
+        } 
+        else {
+            $respuesta = "No es Palindromo";
+        }
+
+        return $respuesta;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
