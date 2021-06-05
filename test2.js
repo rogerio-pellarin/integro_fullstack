6a667c8b9da8ba51edbb4369c6a1d82ef258dc0b
Johan Córdoba <johan732732@gmail.com>
2021-06-04 22:22:26 -0500

    Test 2 done:
    Implement apples(input) function for giving apple box packs to friends

--- test2.js
+++ test2.js
@@ -1,7 +1,36 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function apples(input) {
+  /** Constants */
+  let APPLES_PER_BOX = 4;
+  let BOX_PER_PACK = 2;
+  let PACK_PER_FRIEND = 2;
 
+  let boxes = [];
+  let packages = [];
+  let output = [];
+
+  /** Separate apples in boxes*/
+  while (input.length > 0) {
+    let box = input.splice(0, APPLES_PER_BOX);
+    boxes.push(box);
+  }
+
+  /** Build packages */
+  while (boxes.length > 0) {
+    let pack = boxes.splice(0, BOX_PER_PACK);
+    packages.push(pack);
+  }
+
+  /** Give packages to friend */
+  while (packages.length > 0) {
+    let give_pacakage = packages.splice(0, PACK_PER_FRIEND);
+    output.push(give_pacakage);
+  }
+
+  return JSON.stringify({ output });
 }
 
-console.log(apples(input))
+console.log(apples(input));
