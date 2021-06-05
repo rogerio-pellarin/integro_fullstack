39f92cc2067f58a35b9556c31bb24df9c7b4d802
Johan Córdoba <johan732732@gmail.com>
2021-06-04 20:20:43 -0500

    Test 1 done:
    Add is_palindrome($input) function implementation

--- test1.php
+++ test1.php
@@ -1,7 +1,26 @@
 <?php
 
-function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+function is_palindrome($input)
+{
+    /** Constant variables */
+    $NO_SPACE = '';
+    $REGEX = '/[^A-Za-z0-9\-]/';
+    $IN_CHARSET = "utf-8";
+    $OUT_CHARSET = "ascii//TRANSLIT";
+
+    /** Transform diacritics to base form */
+    $inputBaseForm = iconv($IN_CHARSET, $OUT_CHARSET, $input);
+
+    /** Remove special chars */
+    $stringWithoutSpecialChars = preg_replace($REGEX, $NO_SPACE, $inputBaseForm);
+
+    /** Change to capital letters */
+    $finalInput = strtoupper($stringWithoutSpecialChars);
+
+    /** Reverse the string*/
+    $finalPalindrome = strrev($finalInput);
+
+    return $finalPalindrome == $finalInput;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
