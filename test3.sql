0c7323bae1db8da45a3eff22c05f7de82cd55ca6
Andres Bravo <andres@queo.com.co>
2021-07-01 11:54:14 -0500

    [Fix] Sql query

--- test3.sql
+++ test3.sql
@@ -1,8 +1,9 @@
-select u.name as user_name, u.birthdate, c.name customer_name
+select u.name as user_name, u.birthdate, c.name as customer_name
 from user u
          left join user_customer uc on u.id = uc.user_id
          left join customer c on uc.customer_id = c.id
 where u.birthdate::date <= dateadd('year', -18, current_date)
+and u.status = 1
 and c.status = 1
 and c.id is not null
 order by c.name
