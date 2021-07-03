e6927932d53d4280b813d8336941cad2a6906adb
Mayron urieles <maironurielesan@gmail.com>
2021-07-03 18:31:30 -0500

    test4 react terminado

--- test1.php
+++ test1.php
@@ -16,4 +16,4 @@ function is_palindrome($input) {
     return false;
 }
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
-var_dump(is_palindrome($input));
+var_dump(is_palindrome($input));  
