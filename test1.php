47f02f091e94145b82927ae113c130daf4a7d77d
dante0432 <jgcardenasa@gmail.com>
2021-07-16 12:47:24 -0500

    palindrome function

--- test1.php
+++ test1.php
@@ -1,7 +1,18 @@
 <?php
 
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+  $utf8 = array(
+    '/[áàâãªä]/u'   =>   'a',
+    '/[íìîï]/u'     =>   'i',
+    '/[éèêë]/u'     =>   'e',
+    '/[óòôõºö]/u'   =>   'o',
+    '/[úùûü]/u'     =>   'u',
+  );
+  $inputWithoutAccents = preg_replace(array_keys($utf8), array_values($utf8), $input);
+  $inputSpecials = preg_replace('/[^A-Za-z0-9\-]/', '', $inputWithoutAccents);
+  $InputInUppercase = strtoupper($inputSpecials);
+  $invertString = strrev($InputInUppercase);
+  return strcmp($InputInUppercase, $invertString)==0 ? true :  false;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
