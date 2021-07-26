dd743dbc679f859773ce49728602c713b6ef8a00
Neil Sanchez <nd.sanchezq@gmail.com>
2021-07-25 23:18:04 -0500

    Created react component and webpack setup

--- src/frontend/test4.jsx
+++ src/frontend/test4.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useCallback, useState } from 'react'
+import axios from 'axios'
 
-export default class Palindrome extends React.Component {
+const API_URL = 'http://localhost:8000/src/backend/test1.php';
 
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+const Palindrome = () => {
+  const [string, setString] = useState('')
+  const [message, setMessage] = useState('')
+
+  const onChangeHandle = (event) => setString(event.target.value)
+  const submitHandle = useCallback(() => {
+    const params = new URLSearchParams()
+    params.append('input', string)
+    axios
+      .post(API_URL, params)
+      .then(({ data }) => {
+        console.log('success:', data)
+        if (data.isPalindrome) {
+          setMessage('This is a palindrome')
+        } else {
+          setMessage('This is not a palindrome')
+        }
+      })
+      .catch((error) => {
+        console.log('Error:', error)
+      })
+  }, [string])
+
+  return (
+    <>
+      <input
+        type='text'
+        name='string'
+        id='string-input'
+        placeholder='Type something...'
+        onChange={onChangeHandle}
+        value={string}
+      />
+      <br />
+      <br />
+      <input type='button' value='Validate' onClick={submitHandle} />
+      <input
+        type='button'
+        value='Clean'
+        onClick={() => {
+          setMessage('')
+          setString('')
+        }}
+      />
+      <br />
+      <br />
+      <span>{message}</span>
+    </>
+  )
 }
+
+export default Palindrome
