9fcbd4a4ecc1559dcc7d01d760813263550cbb20
Neil Sanchez <nd.sanchezq@gmail.com>
2021-07-25 23:13:16 -0500

    created method to group arrays

--- src/frontend/utils/test2.js
+++ src/frontend/utils/test2.js
@@ -1,7 +1,26 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-function apples(input) {
+const group = (input, groupByNumber = 2) => {
+  let arrayParent = []
+  let arrayChild = []
+  let index = 1
+  let counter = 1
 
+  while (index <= input.length) {
+    arrayChild.push(input[index - 1])
+    if (index === counter * groupByNumber || index === input.length) {
+      arrayParent.push(arrayChild)
+      arrayChild = []
+      counter++
+    }
+    index++
+  }
+
+  return arrayParent
 }
 
-console.log(apples(input))
+const apples = (input) => group(group(group(input, 4)))
+
+//console.log(apples(input))
+
+module.exports = apples
