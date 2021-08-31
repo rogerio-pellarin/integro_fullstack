05708e75bd046043dd571e7c6228eb86179808cc
Miguel Parra <maparrar@gmail.com>
2021-08-30 23:26:01 -0500

    Create an interface to check if a string is a palindrome

--- test4.jsx
+++ test4.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
-export default class Palindrome extends React.Component {
+const QueryForm = props => {
+  const [string, setString] = useState('');
 
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  const handleInput = e => setString(e.target.value);
+
+  const handleClick = () => props.onCheck(string);
+
+  return <>
+    <input type="text" value={string} onChange={handleInput}/>
+    <button onClick={handleClick}>Check palindrome</button>
+  </>
 }
+
+const Palindrome = () => {
+  const handleCheckPalindrome = string => {
+    axios.post('https://example.com/isPalindrome/', {string: string}).then(response => {
+      if(response.data.isPalindrome) {
+        console.log(`${string} is a palindrome!`);
+      } else {
+        console.log(`${string} is NOT a palindrome!`);
+      }
+    }).catch(error => {
+      console.log("There is an error, please try latter", error);
+    }).finally(() => {
+      console.log("Thanks for using our palindrome service")
+    });
+  }
+
+  return <>
+    <QueryForm onCheck={handleCheckPalindrome}/>
+  </>
+}
+
+export default Palindrome;
