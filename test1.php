f1d026754bd4d0460e0c557cfe67feaff50b9243
Miguel Parra <maparrar@gmail.com>
2021-08-30 20:10:43 -0500

    Solve palindrome problem iterating half of the array

--- test1.php
+++ test1.php
@@ -1,9 +1,20 @@
 <?php
+function clean($string) {
+  $toRemove = array (" ", ",", "á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
+  $replacement= array ("", "", "a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E");
+  return str_replace($toRemove, $replacement , strtolower($string));
+}
 
 function is_palindrome($input) {
-  for($i = 0; $i < len($input); $i++) {
+  $inputCleaned = clean($input);
 
+  for($i = 0; $i < floor(strlen($inputCleaned) / 2); $i++) {
+    if($inputCleaned[$i] !== $inputCleaned[strlen($inputCleaned) -1 - $i]) {
+      return false;
+    }
   }
+
+  return true;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
