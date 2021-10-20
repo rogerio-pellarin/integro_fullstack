354ffe80ea24b24fe776caef921bfc5f942b8569
ttatiana18 <61387311+ttatiana18@users.noreply.github.com>
2021-10-20 00:26:01 -0500

    Desarrollo de las pruebas integro fullstack Nicole Ríos

--- test1.php
+++ test1.php
@@ -2,6 +2,36 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
+
+    $text1 = clean_text($input);
+    $text2 = strrev($text1);
+
+    var_dump($text1);
+    var_dump($text2);
+
+    $response = false; 
+
+    if($text1 == $text2)
+    {
+        $response = true;
+    }
+
+    return $response;
+}
+
+function clean_text($text)
+{
+    //eliminar caracteres adicionales
+    $final_text = str_replace([',',' ','.','-','?','¿','!','¡'],'',$text); 
+
+    //eliminar acentos
+    $final_text = str_replace(['Á','É','Í','Ó','Ú','Ü','Ñ'],['a','e','i','o','u','u','n'],$final_text); 
+    $final_text = str_replace(['á','é','í','ó','ú','ü','ñ'],['a','e','i','o','u','u','n'],$final_text);
+
+    //convertir el texto a minusculas
+    $final_text = strtolower($final_text);
+
+    return $final_text;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
--- test2.js
+++ test2.js
@@ -1,7 +1,57 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 function apples(input) {
 
+    var boxes = []
+    var box = []
+    input.forEach(function(value, index, array) {
+        if ((index + 1) % 4 == 0) {
+            box.push(value)
+            boxes.push(box)
+            box = []
+        } else {
+            box.push(value)
+        }
+    });
+
+    if (box.length < 4 && box.length > 0) {
+        boxes.push(box)
+    }
+
+    var packages = []
+    var package = []
+
+    boxes.forEach(function(value, index, array) {
+        if ((index + 1) % 2 == 0) {
+            package.push(value)
+            packages.push(package)
+            package = []
+        } else {
+            package.push(value)
+        }
+    });
+
+    if (package.length < 2 && package.length > 0) {
+        packages.push(package)
+    }
+
+    var response = []
+    var friend = []
+
+    packages.forEach(function(value, index, array) {
+        if ((index + 1) % 2 == 0) {
+            friend.push(value)
+            response.push(friend)
+            friend = []
+        } else {
+            friend.push(value)
+        }
+    });
+
+    if (friend.length < 2 && friend.length > 0) {
+        response.push(friend)
+    }
+    return response
 }
 
 console.log(apples(input))
