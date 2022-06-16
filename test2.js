f8259257b278014f369cc76c7ccf01012bd1cbf9
emza06adsi <emeza06@misena.edu.co>
2022-06-16 11:33:53 -0500

    test done

--- test2.js
+++ test2.js
@@ -1,7 +1,45 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-
-function apples(input) {
-
-}
-
-console.log(apples(input))
+const input = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25, 26, 27, 28, 29,
+  ];
+  const amount = 3;
+  
+  const apples = (input, amount) => {
+    const mod = parseInt(input?.length % amount);
+  
+    let ouput = {},
+      boxForAmount = 1,
+      friend = 1,
+      array = [],
+      index,
+      numberOfApples = 1;
+  
+    for (index in input) {
+      if (boxForAmount - 1 === amount) {
+        ouput[`friend-${friend}`] = [array];
+        array = [];
+        boxForAmount = 1;
+        friend++;
+      }
+      numberOfApples++;
+      array.push(parseInt(index) + 1);
+      boxForAmount++;
+    }
+  
+    array = [];
+  
+    if (mod > 0) {
+      const actualPosition = input.length - mod;
+  
+      for (let index = actualPosition; index < input.length; index++) {
+        array.push(input[index]);
+      }
+  
+      ouput[`friend-${friend}`] = [array];
+    }
+  
+    return ouput;
+  };
+  
+  console.log(apples(input, amount));
+  
--- test4.jsx
+++ test4.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useState } from "react";
+import axios from "axios";
 
-export default class Palindrome extends React.Component {
+export const Palindrome = () => {
+  const [palindrome, setPalidrome] = useState("");
+  const [isPalindrome, setIsPalindrome] = useState(false);
 
-    render() {
-        return (
-            <>
-            </>
-        )
+  const handlerClick = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://www.integ.ro/api/v1/get-palindrome/?${palindrome}`
+      );
+      setIsPalindrome(data);
+    } catch (err) {
+      setIsPalindrome(false);
+      console.error(err);
     }
-}
+  };
+  return (
+    <>
+      <input
+        value={palindrome}
+        onChange={(e) => {
+          setPalidrome(e.target.value);
+        }}
+        type="text"
+        name="palindrome"
+      />
+      <input type="button" value="action" onClick={handlerClick} />
+
+      {isPalindrome ? <p>true</p> : <p>false</p>}
+    </>
+  );
+};
