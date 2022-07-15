f9a096bd30bd772c38cda3fa348eab17a5d3e51f
grexon-hack <jsarabialugo@gmail.com>
2022-07-15 15:30:06 -0500

    feat(/test1):finalized

--- test1.php
+++ test1.php
@@ -2,7 +2,23 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
+    
+    if (strlen($input)<2) {
+        return false;
+    }
+ 
+    // eliminamos los espacios, comas, puntos y convertimos la cadena en minusculas
+    $input=strtolower(str_replace([" ", ",", "."], "", $input));
+ 
+    for ($i=0;$i<strlen($input);$i++) {
+        if ($input[$i]!=$input[strlen($input)-$i-1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
 var_dump(is_palindrome($input));
+
+?>
