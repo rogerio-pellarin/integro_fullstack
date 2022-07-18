57401a207dbd1da568cfe493a5dcd7e69bdbe9b5
JulianEZT <julianezt3030@gmail.com>
2022-07-18 18:50:44 -0500

    corrección de tildes y mayusculas

--- test1.php
+++ test1.php
@@ -13,5 +13,5 @@ function is_palindrome($input)
     }
 }
 
-$input = "seres";
+$input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
 var_dump(is_palindrome($input));
