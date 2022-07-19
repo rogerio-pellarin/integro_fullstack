26bc476eb981115c7924e864c1abeb274da3a178
Romario <rpbarrios27@gmail.com>
2022-07-19 18:03:36 -0500

    feat/Add form react

--- test1.php
+++ test1.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 
 function eliminar_acentos_simbolos($cadena){
 
@@ -51,13 +53,19 @@ function eliminar_acentos_simbolos($cadena){
 }
 
 function is_palindrome($input) {
-    $newText="";
-    $newInput =  preg_replace("/\s+/", "", eliminar_acentos_simbolos( $input)) ;
-    if($newText == strrev($newText)){
+    $newInput =  strtolower(preg_replace("/\s+/", "", eliminar_acentos_simbolos( $input)));
+    if($newInput == strrev($newInput)){
         return true;
     }
     return false;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
-var_dump(is_palindrome($input));
+
+if(isset($_POST) && !empty($_POST)){
+    $data = json_decode(file_get_contents("php://input"), true);
+    $textInput = $data['textInput'];
+    var_dump(is_palindrome($textInput));
+}else{
+    var_dump(is_palindrome($input));
+}
--- test4.jsx
+++ test4.jsx
@@ -2,11 +2,54 @@ import React from 'react';
 import axios from 'axios';
 
 export default class Palindrome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            result: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        axios({
+            method: 'POST',
+            url: 'http://localhost/test1.php',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                textInput: this.state.value
+            }
+        })
+            .then((response) => {
+                this.setState({ result: response });
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     render() {
         return (
             <>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Frase palindromo:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                {result && <div>result</div>}
             </>
-        )
+
+        );
     }
 }
