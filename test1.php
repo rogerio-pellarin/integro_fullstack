8bc4f9d5726ad8718e7c9fb3e554c7d905c6ad86
Romario <rpbarrios27@gmail.com>
2022-07-19 15:05:43 -0500

    feat/Add new function is_palindromo

--- test1.php
+++ test1.php
@@ -1,7 +1,62 @@
 <?php
 
+function eliminar_acentos_simbolos($cadena){
+
+    //Reemplazamos la , y . por expacios vacios
+    $cadena = str_replace(
+        array(',','.'),
+        array('', '', ),
+        $cadena
+        );
+		
+    //Reemplazamos la A y a
+    $cadena = str_replace(
+    array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+    array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+    $cadena
+    );
+
+    //Reemplazamos la E y e
+    $cadena = str_replace(
+    array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+    array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+    $cadena );
+
+    //Reemplazamos la I y i
+    $cadena = str_replace(
+    array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+    array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+    $cadena );
+
+    //Reemplazamos la O y o
+    $cadena = str_replace(
+    array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+    array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+    $cadena );
+
+    //Reemplazamos la U y u
+    $cadena = str_replace(
+    array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+    array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+    $cadena );
+
+    //Reemplazamos la N, n, C y c
+    $cadena = str_replace(
+    array('Ñ', 'ñ', 'Ç', 'ç'),
+    array('N', 'n', 'C', 'c'),
+    $cadena
+    );
+    
+    return $cadena;
+}
+
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+    $newText="";
+    $newInput =  preg_replace("/\s+/", "", eliminar_acentos_simbolos( $input)) ;
+    if($newText == strrev($newText)){
+        return true;
+    }
+    return false;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
