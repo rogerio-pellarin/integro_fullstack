dff4f96b35fada690afc3305362ce180e4210495
Jhonatan Florez <jflorezlondono@hotmail.com>
2022-07-21 11:40:24 -0500

    Update palindrome in js
    palindrome accents are removed

--- test1.js
+++ test1.js
@@ -1,10 +1,18 @@
-const checkPalindrome = palindrome => {
-    let re = /[\W_]/g;
-    let palindromeLower = palindrome.toLowerCase().replace(re, '');
-    let reverse = palindromeLower.split('').reverse().join(''); 
-    return reverse === palindromeLower;
-}
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  } 
 
-const input = "Alli por la tropa portado, traido a ese paraje de maniobras, una tipa como capitan usar boina me dejara, pese a odiar toda tropa por tal ropilla";
+const checkPalindrome = (palindrome) => {
+  let re = /[\W_]/g;
+  let palindromeLower = removeAccents(palindrome)
+    .toLowerCase()
+    .replace(re, "");
+    console.log(palindromeLower)
+  let reverse = palindromeLower.split("").reverse().join("");
+  return reverse === palindromeLower;
+};
 
-console.log(checkPalindrome(input))
+const input =
+  "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
+
+console.log(checkPalindrome(input));
